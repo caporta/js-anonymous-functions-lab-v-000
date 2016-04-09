d0a97f24afcbf5f4ffbde2280817b23c666813d0
Chris Aporta <christopher.aporta@gmail.com>
2016-04-09 18:30:34 -0400

    complete lab

--- js/jukebox.js
+++ js/jukebox.js
@@ -5,40 +5,48 @@ $(document).ready(function(){
   show();
 });
 
-var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
-var artists = Object.keys(jukeboxSongs);
-var theArtist = artists[Math.floor(Math.random()) * artists.length];
-var songToPlay = jukeboxSongs[theArtist];
+var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"},
+    artists = Object.keys(jukeboxSongs),
+    theArtist = artists[Math.floor(Math.random()) * artists.length],
+    songToPlay = jukeboxSongs[theArtist];
 
 function randomNumber() {
   return Math.floor(Math.random());
 }
 
-function menu() {
-  $('#menu').on('click', function(e) {
+function showOptions() {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+};
+
+function menu() {
+  $('#menu').on('click', showOptions);
+}
+
+function nowPlaying() {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', nowPlaying);
+}
+
+function pauseSong() {
+  $('#songPaused').html(songToPlay + " is paused");
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', pauseSong);
+}
+
+function displaySongs() {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  $('#showSongs').html(songs);
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click', displaySongs);
 }
